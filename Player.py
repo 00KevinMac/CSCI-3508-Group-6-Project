3af5d3d757a3c775f2f26c9ce91185cec7d3338c
Kevin Macfarlane <guitaringkev@gmail.com>
2019-04-11 18:35:52 -0600

    Added basic error checking

--- Player.py
+++ Player.py
@@ -1,8 +1,17 @@
+import sys
 import random
 import Board
 
 
 def random_move(board, playerNum):
     moves = []
+
+    sys.stderr.write("Board: " + board + '\n')  # unsure if this will work
+
     moves = board.get_OkayDokayColumns()
-    return random.choice(moves)
+    sys.stderr.write("Valid moves: " + moves + '\n')  # displays the valid moves
+
+    column = random.choice(moves)
+    sys.stderr.write("Random move: " + str(column) + '\n')  # displays the random column choice
+
+    return column

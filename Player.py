0faba44ae822f9f1958af599a433d844564e865b
happyklans <bstanton4@student.cccs.edu>
2019-05-01 11:53:33 -0600

    Update Player.py

--- Player.py
+++ Player.py
@@ -32,57 +32,10 @@ def same_column_move(board, playerNum):
 
     return column
 
-def brute_force_move(board, playerNum):
 
-    friendly_runs = {}
 
-    hostile_runs = []
-
-    useable_cols = board.get_OkayDokeyColumns()
-
-    #get vert runs
-    for i in range(board.get_length(), 0, -1):
-        for j in range(0, board.get_width()):
-            if board.get_Content(i, j) == playerNum:
-                friendly_runs["run"+str(j)] = 1
-                temp = i 
-                while temp + 1 < board.get_length():
-                    if board.get_Content(temp+1, j) == playerNum:
-                        friendly_runs["run"+str(j)] += 1
-                    else:
-                        break
-                    temp += 1
-            elif board.get_Content(i, j) != playerNum and board.get_Content(i, j) != 0:
-                hostile_runs["run"+str(j)] = 1
-                temp = i 
-                while temp + 1 < board.get_length():
-                    if board.get_Content(i, j) != playerNum and board.get_Content(i, j) != 0:
-                        hostile_runs["run"+str(j)] += 1
-                    else:
-                        break
-                    temp += 1
-
-    #get horiz runs        
-    for i in range(0, board.get_width()):
-        for j in range(board.get_length(), 0, -1):
-            if board.get_Content(i, j) == playerNum:
-                friendly_runs["run"+str(j)] = 1
-                temp = i 
-                while temp + 1 < board.get_length():
-                    if board.get_Content(temp+1, j) == playerNum:
-                        friendly_runs["run"+str(j)] += 1
-                    else:
-                        break
-                    temp += 1
-            elif board.get_Content(i, j) != playerNum and board.get_Content(i, j) != 0:
-                hostile_runs["run"+str(j)] = 1
-                temp = i 
-                while temp + 1 < board.get_length():
-                    if board.get_Content(i, j) != playerNum and board.get_Content(i, j) != 0:
-                        hostile_runs["run"+str(j)] += 1
-                    else:
-                        break
-                    temp += 1
+    
+    
     
                         
     
